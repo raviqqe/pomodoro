e2ac9a2d4c15ea2417c061baf3b916c557ea0813
Yota Toyama <raviqqe@gmail.com>
2019-08-24 14:29:50 -0700

    Show no graph message

--- src/infrastructure/react/PerformanceGraph.tsx
+++ src/infrastructure/react/PerformanceGraph.tsx
@@ -10,26 +10,34 @@ const Container = styled.div`
   height: 80vh;
 `;
 
+const NoGraphMessage = styled.div`
+  color: white;
+  font-size: 1.5em;
+`;
+
 export interface IProps {
   performanceGraph: IPerformanceGraph;
 }
 
-export const PerformanceGraph = ({ performanceGraph }: IProps) => (
-  <Container>
-    <ResponsiveContainer>
-      <BarChart data={performanceGraph.data}>
-        <XAxis
-          dataKey="date"
-          tickFormatter={date =>
-            DateTime.fromJSDate(
-              DateSerializer.deserialize(date)
-            ).toLocaleString({ day: "numeric", month: "long" })
-          }
-          tickMargin={10}
-        />
-        <YAxis tickMargin={5} />
-        <Bar dataKey="pomodoros" fill="salmon" />
-      </BarChart>
-    </ResponsiveContainer>
-  </Container>
-);
+export const PerformanceGraph = ({ performanceGraph }: IProps) =>
+  performanceGraph.data.length === 0 ? (
+    <NoGraphMessage>No performance graph to show yet!</NoGraphMessage>
+  ) : (
+    <Container>
+      <ResponsiveContainer>
+        <BarChart data={performanceGraph.data}>
+          <XAxis
+            dataKey="date"
+            tickFormatter={date =>
+              DateTime.fromJSDate(
+                DateSerializer.deserialize(date)
+              ).toLocaleString({ day: "numeric", month: "long" })
+            }
+            tickMargin={10}
+          />
+          <YAxis tickMargin={5} />
+          <Bar dataKey="pomodoros" fill="salmon" />
+        </BarChart>
+      </ResponsiveContainer>
+    </Container>
+  );
