d27f2eeb7fdf6300ec20c2f0555e60fca82fdf0e
Yota Toyama <raviqqe@gmail.com>
2019-09-11 20:37:10 -0700

    Don't close notifications automatically

--- src/infrastructure/notification/builtin-notification-presenter.ts
+++ src/infrastructure/notification/builtin-notification-presenter.ts
@@ -3,8 +3,7 @@ import { INotificationPresenter } from "../../application/notification-presenter
 export class BuiltinNotificationPresenter implements INotificationPresenter {
   public presentNotification(message: string): void {
     if (Notification.permission === "granted") {
-      const notification = new Notification(message, { icon: "/icon192.png" });
-      setTimeout(() => notification.close(), 6000);
+      new Notification(message, { icon: "/icon192.png" });
     }
   }
 }
