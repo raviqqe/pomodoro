0bbe9a8469af91c908c10f13b4fb92f95f3169d5
Yota Toyama <raviqqe@gmail.com>
2022-04-23 04:08:51 +0000

    Fix tests

--- src/infrastructure/react/__tests__/App.tsx
+++ src/infrastructure/react/__tests__/App.tsx
@@ -1,4 +1,4 @@
-import { act, render, waitFor } from "@testing-library/react";
+import { act, render, RenderResult, waitFor } from "@testing-library/react";
 import { PomodoroTimerState } from "../../../application/pomodoro-timer-state";
 import { App, IProps } from "../App";
 
@@ -24,31 +24,37 @@ const props: IProps = {
 };
 
 it("renders before a user signs in", async () => {
-  await act(async () => {
-    expect(
-      render(<App {...props} signedIn={null} />).container
-    ).toMatchSnapshot();
+  let result: RenderResult | undefined;
 
-    await wait();
+  act(() => {
+    result = render(<App {...props} signedIn={null} />);
   });
+
+  expect(result?.container).toMatchSnapshot();
+
+  await wait();
 });
 
 it("renders after a user signs in", async () => {
-  await act(async () => {
-    expect(
-      render(<App {...props} signedIn={true} />).container
-    ).toMatchSnapshot();
+  let result: RenderResult | undefined;
 
-    await wait();
+  act(() => {
+    result = render(<App {...props} signedIn={true} />);
   });
+
+  expect(result?.container).toMatchSnapshot();
+
+  await wait();
 });
 
 it("renders after a user signs out", async () => {
-  await act(async () => {
-    expect(
-      render(<App {...props} signedIn={false} />).container
-    ).toMatchSnapshot();
+  let result: RenderResult | undefined;
 
-    await wait();
+  act(() => {
+    result = render(<App {...props} signedIn={false} />);
   });
+
+  expect(result?.container).toMatchSnapshot();
+
+  await wait();
 });
