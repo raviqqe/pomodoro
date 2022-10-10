4c2812186ccdb4acb8490694e25c1c1afe2bd3ff
Yota Toyama <raviqqe@gmail.com>
2022-10-10 12:32:13 -0700

    Use Vite

---
+++ vite.config.ts
@@ -0,0 +1,24 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
+
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        short_name: "Pomodoro",
+        name: "Pomodoro",
+        icons: [{ src: "icon.svg", sizes: "any" }],
+        start_url: ".",
+        display: "standalone",
+        theme_color: "salmon",
+        background_color: "paleturquoise",
+      },
+      workbox: {
+        navigateFallbackDenylist: [/^\/__/],
+      },
+    }),
+  ],
+  test: { environment: "jsdom" },
+});
