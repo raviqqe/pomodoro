f1fcb4ffd246ab970dca7d7ca93b3bf0633b3da5
Yota Toyama <raviqqe@gmail.com>
2022-12-19 10:18:35 -0800

    Fix bug (#1823)

--- src/index.ts
+++ src/index.ts
@@ -64,8 +64,6 @@ async function main() {
     new SignOutManager(authenticationController, authenticationPresenter),
     configuration.repositoryUrl
   ).render();
-
-  await navigator.serviceWorker.register("/service-worker.js");
 }
 
 main().catch((error: Error) => errorReporter.report(error));
