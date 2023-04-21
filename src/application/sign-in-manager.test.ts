49fe2c032f3fd2332fc9f7573190a2a7d7970f5f
Yota Toyama <raviqqe@gmail.com>
2023-04-21 12:41:32 +1000

    Remove index.ts

--- src/application/sign-in-manager.test.ts
+++ src/application/sign-in-manager.test.ts
@@ -1,9 +1,6 @@
 import { expect, it } from "vitest";
 import { SignInManager } from "./sign-in-manager.js";
-import {
-  authenticationPresenter,
-  authenticationController,
-} from "./test/index.js";
+import { authenticationPresenter, authenticationController } from "./test.js";
 
 it("signs in", async () => {
   const signInManager = new SignInManager(
---
+++ src/infrastructure/react.test.tsx
@@ -0,0 +1,22 @@
+import { it, vi } from "vitest";
+import { type ApplicationInitializer } from "../application/application-initializer.js";
+import { type PerformanceGraphViewer } from "../application/performance-graph-viewer.js";
+import { type PomodoroTimerStarter } from "../application/pomodoro-timer-starter.js";
+import { type PomodoroTimerStopper } from "../application/pomodoro-timer-stopper.js";
+import { type SignInManager } from "../application/sign-in-manager.js";
+import { type SignOutManager } from "../application/sign-out-manager.js";
+import { ReactRenderer } from "./react.js";
+
+it("renders", () => {
+  new ReactRenderer(
+    document.createElement("div"),
+    [],
+    { initialize: vi.fn(async () => {}) } as unknown as ApplicationInitializer,
+    {} as PerformanceGraphViewer,
+    {} as PomodoroTimerStarter,
+    {} as PomodoroTimerStopper,
+    {} as SignInManager,
+    {} as SignOutManager,
+    ""
+  ).render();
+});
