63f49fb9b9a29922c703be1271e37c2012ddbe3f
Yota Toyama <raviqqe@gmail.com>
2023-12-04 21:48:14 +0900

    Use dimgray

--- src/infrastructure/react/style/colors.ts
+++ src/infrastructure/react/style/colors.ts
@@ -1,6 +1,6 @@
 export const black = "#222";
 export const blue = "steelblue";
-export const grey = "darkgrey";
+export const grey = "dimgray";
 export const lightBlue = "steelblue";
 export const red = "tomato";
 export const white = "white";
