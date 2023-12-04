d9ab1517058646b8d9e4e1bb963a9ce926bc4b7d
Yota Toyama <raviqqe@gmail.com>
2023-12-04 21:49:56 +0900

    Change icon color

--- src/infrastructure/react/Landing.tsx
+++ src/infrastructure/react/Landing.tsx
@@ -2,7 +2,7 @@ import { defaultImport } from "default-import";
 import { FaGithub } from "react-icons/fa/index.js";
 import defaultStyled from "styled-components";
 import { SignIn } from "./SignIn.js";
-import { white, black } from "./style/colors.js";
+import { white } from "./style/colors.js";
 
 const styled = defaultImport(defaultStyled);
 
@@ -31,7 +31,7 @@ const Title = styled.div`
 const GitHubLink = styled.a`
   font-size: 2.5rem;
   margin: 1.5rem;
-  color: ${black};
+  color: ${white};
   display: block;
   position: fixed;
   bottom: 0em;
