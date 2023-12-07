629f869a7e0fe309a4d8a2c08e9b8d34d46a573b
Yota Toyama <raviqqe@gmail.com>
2023-12-07 19:09:34 +0900

    Fix

--- src/infrastructure/react/style.ts
+++ src/infrastructure/react/style.ts
@@ -18,6 +18,6 @@ export const globalStyle = css`
   }
 `;
 
-export const boxShadow = css`
-  box-shadow: 0rem 0.2rem 0.2rem rgba(0, 0, 0, 0.2);
-`;
+export const boxShadow = {
+  boxShadow: "0rem 0.2rem 0.2rem rgba(0, 0, 0, 0.2)",
+};
