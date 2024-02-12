977cc30b2bd45d99256ee8c5c2ae1e30c55a46fe
Yota Toyama <raviqqe@gmail.com>
2024-02-12 15:35:06 +0900

    Fix global style (#2422)

--- src/infrastructure/react/style.ts
+++ src/infrastructure/react/style.ts
@@ -1,19 +1,20 @@
-import css from "noop-tag";
+import { css } from "@linaria/core";
 import { black, lightBlue } from "./style/colors.js";
 
-// TODO Use a `css` tag from `@linaria/core`.
 export const globalStyle = css`
-  body {
-    background: ${lightBlue};
-    margin: 0;
-    padding: 0;
-    color: ${black};
-    font-family: Roboto, sans-serif;
-    line-height: 1.4;
-    font-size: 16px;
-    height: 100vh;
-    width: 100vw;
-    overflow: hidden;
+  :global() {
+    body {
+      background: ${lightBlue};
+      margin: 0;
+      padding: 0;
+      color: ${black};
+      font-family: Roboto, sans-serif;
+      line-height: 1.4;
+      font-size: 16px;
+      height: 100vh;
+      width: 100vw;
+      overflow: hidden;
+    }
   }
 `;
 
