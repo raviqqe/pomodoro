20e299d65d4f6fa98185d39622da0adc0edf5fac
Yota Toyama <raviqqe@gmail.com>
2024-12-06 22:31:47 +0900

    Fix spell check (#2843)

--- src/main/pomodoro-timer-starter.ts
+++ src/main/pomodoro-timer-starter.ts
@@ -1,5 +1,5 @@
 import { PomodoroTimerStarter } from "../application/pomodoro-timer-starter.js";
-import { pomodoroTimer } from "../maiin/pomodoro-timer.js";
+import { pomodoroTimer } from "../main/pomodoro-timer.js";
 import { notificationController } from "./notification-controller.js";
 
 export const pomodoroTimerStarter = new PomodoroTimerStarter(
