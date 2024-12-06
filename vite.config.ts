8695427380f767965924b8c5a9c0dbf94010e630
Yota Toyama <raviqqe@gmail.com>
2024-12-06 14:10:56 +0900

    Refactor (#2831)
    - **Fix**
    - **Fix**
    - **Fix**

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,12 @@
-import linaria from "@wyw-in-js/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
-import { UserConfigExport } from "vitest/config";
 import { VitePWA } from "vite-plugin-pwa";
+import wywInJS from "@wyw-in-js/vite";
 
 export default defineConfig({
   plugins: [
     react(),
-    linaria({
+    (wywInJS as unknown as Function)({
       include: ["src/**/*.{ts,tsx}"],
       babelOptions: {
         presets: ["@babel/preset-typescript", "@babel/preset-react"],
@@ -28,9 +27,4 @@ export default defineConfig({
       },
     }),
   ],
-  test: {
-    environment: "jsdom",
-    include: ["src/**/*.test.ts{,x}"],
-    setupFiles: "src/test.ts",
-  },
-} satisfies UserConfigExport);
+});
