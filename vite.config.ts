4ae14dd881f98f5a00e6ab9ad107a6278e0c2cce
Yota Toyama <raviqqe@gmail.com>
2024-12-06 14:06:50 +0900

    Fix

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
-import linaria from "@wyw-in-js/vite";
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import { VitePWA } from "vite-plugin-pwa";
+import wywInJS from "@wyw-in-js/vite";
 
 export default defineConfig({
   plugins: [
     react(),
-    linaria({
+    (wywInJS as unknown as Function)({
       include: ["src/**/*.{ts,tsx}"],
       babelOptions: {
         presets: ["@babel/preset-typescript", "@babel/preset-react"],
