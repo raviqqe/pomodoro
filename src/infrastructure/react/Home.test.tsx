82ad7b8caff40041010f568e6832fd101bd6fce2
Yota Toyama <raviqqe@gmail.com>
2024-12-07 10:37:26 +0900

    Fix

--- src/infrastructure/react/Home.test.tsx
+++ src/infrastructure/react/Home.test.tsx
@@ -1,75 +1,66 @@
-import {
-  act,
-  fireEvent,
-  render,
-  type RenderResult,
-  waitFor,
-} from "@testing-library/react";
-import { expect, it } from "vitest";
+import { act, fireEvent, render, waitFor } from "@testing-library/react";
+import { beforeEach, expect, it, vi } from "vitest";
 import { PomodoroTimerState } from "../../application/pomodoro-timer-state.js";
 import { Home, type Props } from "./Home.js";
+import { performanceGraphViewer } from "../../main/performance-graph-viewer.js";
 
 const props: Props = {
   performanceGraph: { data: [] },
   timer: { seconds: 42, state: PomodoroTimerState.Pomodoro, stopped: false },
 };
 
+beforeEach(() => {
+  vi.spyOn(performanceGraphViewer, "viewGraph").mockResolvedValue();
+});
+
 it("renders", () => {
   expect(render(<Home {...props} />).container.firstChild).toMatchSnapshot();
 });
 
 it("views a performance graph", async () => {
-  let result: RenderResult | undefined;
-
-  act(() => {
-    result = render(<Home {...props} />);
-  });
+  const result = await act(async () => render(<Home {...props} />));
 
   act(() => {
     fireEvent.click(
-      result?.container.querySelector('[aria-label="View Graph"]')!,
+      result.container.querySelector('[aria-label="View Graph"]')!,
     );
   });
 
   await waitFor(() =>
     expect(
-      result?.container.querySelector('[aria-label="View Timer"]'),
+      result.container.querySelector('[aria-label="View Timer"]'),
     ).toBeTruthy(),
   );
 
-  expect(result?.container).toMatchSnapshot();
+  expect(result.container).toMatchSnapshot();
 });
 
 it("goes back to a timer view", async () => {
-  let result: RenderResult | undefined;
-
-  act(() => {
-    result = render(<Home {...props} />);
-  });
+  const result = await act(async () => render(<Home {...props} />));
 
   act(() => {
     fireEvent.click(
-      result?.container.querySelector('[aria-label="View Graph"]')!,
+      result.container.querySelector('[aria-label="View Graph"]')!,
     );
   });
 
   await waitFor(() =>
     expect(
-      result?.container.querySelector('[aria-label="View Timer"]'),
+      result.container.querySelector('[aria-label="View Timer"]'),
     ).toBeTruthy(),
   );
 
   act(() => {
     fireEvent.click(
-      result?.container.querySelector('[aria-label="View Timer"]')!,
+      result.container.querySelector('[aria-label="View Timer"]')!,
     );
   });
 
   await waitFor(() =>
     expect(
-      result?.container.querySelector('[aria-label="View Graph"]'),
+      result.container.querySelector('[aria-label="View Graph"]'),
     ).toBeTruthy(),
   );
 
-  expect(result?.container).toMatchSnapshot();
+  expect(result.container).toMatchSnapshot();
 });
