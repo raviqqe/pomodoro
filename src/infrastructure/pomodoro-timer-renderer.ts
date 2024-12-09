d1bd49f3d1d5d1de5c375462733b03b69b7f082a
Yota Toyama <raviqqe@gmail.com>
2024-12-09 20:25:44 +0900

    Fix

--- src/infrastructure/pomodoro-timer-renderer.ts
+++ src/infrastructure/pomodoro-timer-renderer.ts
@@ -1,34 +1,24 @@
+import { map } from "nanostores";
 import { type PomodoroTimerPresenter } from "../application/pomodoro-timer-presenter.js";
 import { PomodoroTimerState } from "../application/pomodoro-timer-state.js";
-import { type PomodoroTimer, type Renderer } from "./renderer.js";
+import { type PomodoroTimer } from "./renderer.js";
 
 export class PomodoroTimerRenderer implements PomodoroTimerPresenter {
-  private renderer: Renderer | null = null;
-  private timer: PomodoroTimer = {
+  public readonly timer = map<PomodoroTimer>({
     seconds: 0,
     state: PomodoroTimerState.Pomodoro,
     stopped: true,
-  };
-
-  public setRenderer(renderer: Renderer): void {
-    this.renderer = renderer;
-  }
+  });
 
   public presentTime(seconds: number): void {
-    this.renderTimer({ seconds });
+    this.timer.setKey("seconds", seconds);
   }
 
   public presentState(state: PomodoroTimerState): void {
-    this.renderTimer({ state });
+    this.timer.setKey("state", state);
   }
 
   public presentStopped(stopped: boolean): void {
-    this.renderTimer({ stopped });
-  }
-
-  private renderTimer(timer: Partial<PomodoroTimer>): void {
-    this.timer = { ...this.timer, ...timer };
-
-    this.renderer?.renderTimer(this.timer);
+    this.timer.setKey("stopped", stopped);
   }
 }
