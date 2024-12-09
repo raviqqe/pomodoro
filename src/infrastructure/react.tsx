b1972041aafaedcd0e5658f85609616ac1cb0db3
Yota Toyama <raviqqe@gmail.com>
2024-12-09 20:47:56 +0900

    Fix

--- src/infrastructure/react.tsx
+++ src/infrastructure/react.tsx
@@ -3,13 +3,10 @@ import { createRoot } from "react-dom/client";
 import { App } from "./react/App.js";
 import { globalStyle } from "./react/style.js";
 
-export const render = (element: HTMLElement): void => {
-  const root = createRoot(element);
-
-  root.render(
+export const render = (element: HTMLElement): void =>
+  createRoot(element).render(
     <StrictMode>
       <style className={globalStyle} />
       <App />
     </StrictMode>,
   );
-};
