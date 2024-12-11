6b0e1c111a643e2be09222ecd0f7cefe3b45a3c8
Yota Toyama <raviqqe@gmail.com>
2024-12-11 19:34:01 +0900

    Reafctor routes (#2857)
    - **Fix**
    - **Fix**
    - **Fix**
    - **Fix**

--- src/app/routes/timer.tsx
+++ src/app/routes/timer.tsx
@@ -1,4 +1,48 @@
-import { type JSX } from "react";
-import { Home } from "../components/Home.js";
+import { styled } from "@linaria/react";
+import { useState } from "react";
+import { performanceGraphViewer } from "../../main/performance-graph-viewer.js";
+import { PerformanceGraph } from "../components/PerformanceGraph.js";
+import { SignOut } from "../components/SignOut.js";
+import { Timer } from "../components/Timer.js";
+import { ViewGraph } from "../components/ViewGraph.js";
+import { ViewTimer } from "../components/ViewTimer.js";
 
-export default (): JSX.Element => <Home />;
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Buttons = styled.div`
+  position: fixed;
+  top: 0.5em;
+  right: 0.5em;
+
+  > :not(:last-child) {
+    margin-bottom: 0.5em;
+  }
+`;
+
+export default (): JSX.Element => {
+  const [graphViewed, setGraphViewed] = useState(false);
+
+  return (
+    <Container>
+      {graphViewed ? <PerformanceGraph /> : <Timer />}
+      <Buttons>
+        <SignOut />
+        {graphViewed ? (
+          <ViewTimer onClick={() => setGraphViewed(false)} />
+        ) : (
+          <ViewGraph
+            onClick={async () => {
+              await performanceGraphViewer.viewGraph();
+              setGraphViewed(true);
+            }}
+          />
+        )}
+      </Buttons>
+    </Container>
+  );
+};
