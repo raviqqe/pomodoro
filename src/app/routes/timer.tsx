10ed69cb4d283a45e06bd5430c8a67608ee65363
Yota Toyama <raviqqe@gmail.com>
2024-12-11 20:12:52 +0900

    Fix

--- src/app/routes/timer.tsx
+++ src/app/routes/timer.tsx
@@ -1,37 +1,3 @@
-import { styled } from "@linaria/react";
-import { SignOut } from "../components/SignOut.js";
 import { Timer } from "../components/Timer.js";
-import { ViewGraphButton } from "../components/ViewGraphButton.js";
-import { ViewTimerButton } from "../components/ViewTimerButton.js";
-import { useLocation } from "react-router";
 
-const Container = styled.div`
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  height: 100vh;
-`;
-
-const Buttons = styled.div`
-  position: fixed;
-  top: 0.5em;
-  right: 0.5em;
-
-  > :not(:last-child) {
-    margin-bottom: 0.5em;
-  }
-`;
-
-export default (): JSX.Element => {
-  const { pathname } = useLocation();
-
-  return (
-    <Container>
-      <Timer />
-      <Buttons>
-        <SignOut />
-        {pathname === "/timer" ? <ViewGraphButton /> : <ViewTimerButton />}
-      </Buttons>
-    </Container>
-  );
-};
+export default (): JSX.Element => <Timer />;
