68acad8bab532837f3176fa095c765e26ec994ef
Yota Toyama <raviqqe@gmail.com>
2024-12-11 20:29:45 +0900

    Refactor routes (#2859)
    - **Fix**
    - **Fix**
    - **Fix**

--- src/app/routes/performance.tsx
+++ src/app/routes/performance.tsx
@@ -1,4 +1,65 @@
-import { type JSX } from "react";
-import { PerformanceGraph } from "../components/PerformanceGraph.js";
+import { styled } from "@linaria/react";
+import { useStore } from "@nanostores/react";
+import { last } from "@raviqqe/loscore";
+import { differenceInDays } from "date-fns";
+import {
+  Bar,
+  BarChart,
+  CartesianGrid,
+  Label,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from "recharts";
+import { DateSerializer } from "../../domain/date-serializer.js";
+import { performanceGraphPresenter } from "../../main/performance-graph-presenter.js";
+import { grey, red, white } from "../style.js";
 
-export default (): JSX.Element => <PerformanceGraph />;
+const Container = styled.div`
+  width: 80vw;
+  height: 80vh;
+`;
+
+const Message = styled.div`
+  color: ${white};
+  font-size: 1.5em;
+`;
+
+export default (): JSX.Element => {
+  const { data } = useStore(performanceGraphPresenter.graph);
+  const lastDatum = last(data);
+
+  return lastDatum ? (
+    <Container>
+      <ResponsiveContainer>
+        <BarChart data={data}>
+          <CartesianGrid fill={white} stroke={grey} strokeDasharray="3 3" />
+          <XAxis
+            dataKey="date"
+            stroke={white}
+            tickFormatter={(date: string): string => {
+              const days: number = differenceInDays(
+                DateSerializer.deserialize(lastDatum.date),
+                DateSerializer.deserialize(date),
+              );
+
+              return days === 0 ? "Today" : `${days} days ago`;
+            }}
+            tickMargin={10}
+          />
+          <YAxis allowDecimals={false} stroke={white} tickMargin={5}>
+            <Label
+              angle={-90}
+              position="insideLeft"
+              style={{ fill: white }}
+              value="Pomodoros"
+            />
+          </YAxis>
+          <Bar dataKey="pomodoros" fill={red} />
+        </BarChart>
+      </ResponsiveContainer>
+    </Container>
+  ) : (
+    <Message>No performance graph to show yet!</Message>
+  );
+};
--- src/app/routes/timer.tsx
+++ src/app/routes/timer.tsx
@@ -1,3 +1,69 @@
-import { Timer } from "../components/Timer.js";
+import { styled } from "@linaria/react";
+import { useStore } from "@nanostores/react";
+import { PomodoroTimerState } from "../../application/pomodoro-timer-state.js";
+import { pomodoroTimerPresenter } from "../../main/pomodoro-timer-presenter.js";
+import { pomodoroTimerStarter } from "../../main/pomodoro-timer-starter.js";
+import { pomodoroTimerStopper } from "../../main/pomodoro-timer-stopper.js";
+import { TextButton } from "../components/TextButton.js";
+import { white } from "../style.js";
 
-export default (): JSX.Element => <Timer />;
+const Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  > * {
+    margin: 1rem;
+  }
+`;
+
+const State = styled.div`
+  font-size: 5em;
+`;
+
+const Time = styled.div`
+  color: ${white};
+  font-size: 10em;
+`;
+
+const Minutes = styled.span``;
+
+const Seconds = styled.span`
+  font-size: 0.6em;
+  margin-left: 0.2ex;
+`;
+
+export default (): JSX.Element => {
+  const stopped = useStore(pomodoroTimerPresenter.stopped);
+  const state = useStore(pomodoroTimerPresenter.state);
+  const seconds = useStore(pomodoroTimerPresenter.seconds);
+
+  return (
+    <Container>
+      {stopped ? (
+        <>
+          <State>
+            {state === PomodoroTimerState.Pomodoro
+              ? "🍅"
+              : state === PomodoroTimerState.ShortBreak
+                ? "🛌"
+                : "🛌🛌"}
+          </State>
+          <TextButton onClick={() => pomodoroTimerStarter.start()}>
+            Start
+          </TextButton>
+        </>
+      ) : (
+        <>
+          <Time>
+            <Minutes>{Math.floor(seconds / 60)}</Minutes>
+            <Seconds>{seconds % 60}</Seconds>
+          </Time>
+          <TextButton onClick={() => pomodoroTimerStopper.stop()} secondary>
+            Stop
+          </TextButton>
+        </>
+      )}
+    </Container>
+  );
+};
