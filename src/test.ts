0b55073649571170751eebcd2c29a32fe9d73a2d
Yota Toyama <raviqqe@gmail.com>
2026-01-13 20:32:08 -0800

    Remove dead utilities (#3393)

--- src/test.ts
+++ src/test.ts
@@ -1,9 +1,5 @@
 import { cleanup } from "@testing-library/react";
-import { defaultImport } from "default-import";
-import ResizeObserver from "resize-observer-polyfill";
 import { afterEach, vi } from "vitest";
-
-window.ResizeObserver = defaultImport(ResizeObserver);
 
 afterEach(() => {
   cleanup();
